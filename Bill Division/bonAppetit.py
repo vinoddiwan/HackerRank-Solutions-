b988b7c94262d732b8e00c126a08a1ec4a60e2d9
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-02-27 16:25:09 +0530

    Update bonAppetit.py

--- Bill Division/bonAppetit.py
+++ Bill Division/bonAppetit.py
@@ -1,1 +1,13 @@
-
+def bonAppetit(bill, k, b):
+    annaBill = 0
+    for i in range(len(bill)):
+        if i != k: # included only consumed food
+            annaBill += bill[i]
+            
+    # split share food into two
+    annaBill //= 2
+    
+    if annaBill == b: # right calculation
+        print('Bon Appetit')
+    else: # extra taken
+        print(b-annaBill)
