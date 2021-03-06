92919d1b4c75f19170d9a5ebbbc09c076a2d3004
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-06 19:09:31 +0530

    Update rotateLeft.py

--- Left Rotation/rotateLeft.py
+++ Left Rotation/rotateLeft.py
@@ -1,1 +1,12 @@
+# gives TLE on test case 8, 9
+def rotate(arr):
+    first = arr[0]
+    for i in range(len(arr)-1):
+        arr[i] = arr[i+1]
+    arr[len(arr)-1] = first
 
+# main function
+def rotateLeft(d, arr):
+    for _ in range(d):
+        rotate(arr) # function above
+    return arr
